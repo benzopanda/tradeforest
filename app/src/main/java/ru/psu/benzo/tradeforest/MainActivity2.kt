d8c04a7106e79b0c7553fd0e4effbd8736691b3b
BlueQueen <dethwingomg@gmail.com>
2023-12-06 23:15:20 +0500

    Стартовый код для TradeForest.2. Вызов дочерней активности без ожидания результата.

--- app/src/main/java/ru/psu/benzo/tradeforest/MainActivity2.kt
+++ app/src/main/java/ru/psu/benzo/tradeforest/MainActivity2.kt
@@ -2,10 +2,27 @@ package ru.psu.benzo.tradeforest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val bundle = getIntent().getExtras();
+
+        bundle?.let{
+            Toast.makeText(
+                this,
+                bundle.getString("MY_PARAM1", "NO PARAM"),
+                Toast.LENGTH_LONG
+            )?.show()
+        }?:run{
+            Toast.makeText(
+                this,
+                "PARAM_DENIED",
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 }
